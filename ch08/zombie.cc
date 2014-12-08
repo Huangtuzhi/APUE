5d8c9bbf82ee37d3d692f8d5e4e09e4e35fab18c
小逸 <heying1991@gmail.com>
2014-12-08 10:35:28 +0800

    Update zombie.cc

--- ch08/zombie.cc
+++ ch08/zombie.cc
@@ -8,17 +8,21 @@ int main()
     if ((pid = fork()) < 0) {
         err_sys("fork error");
     } else if (pid == 0) {
+        //父进程A的子进程B
         if ((pid = fork()) < 0) 
             err_sys("fork error");
         else if (pid > 0){
+            //父进程A的子进程B退出，这里的子进程ID > 0
             printf("parent of the first child process, pid = %d\n",pid);
             exit(0);
         }
 
+        //子进程B的子进程C继续
         sleep(2);
         printf("second child, parent pid = %d\n", getppid());
         exit(0);
     }
+    //父进程A阻塞
     if (waitpid(pid, NULL, 0) != pid)
         err_sys("waitpid error");
     exit(0);
