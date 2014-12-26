ac20a1cb04cd3ee3985ed55a2433d56d5476ec48
Huangtuzhi <heying1991@gmail.com>
2014-12-26 09:51:58 +0800

    m

--- ch13/single_daemon.c
+++ ch13/single_daemon.c
@@ -6,6 +6,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include "../apue.h"
 
 #define LOCKFILE "/var/run/daemon.pid"
 #define LOCKMODE (S_IRUSR |S_IWUSR |S_IRGRP |S_IROTH)
@@ -19,15 +20,18 @@ int already_running(void)
     fd = open(LOCKFILE, O_RDWR|O_CREAT, LOCKMODE);
     if(fd < 0){
         syslog(LOG_ERR, "can't open %s: %s", LOCKFILE, strerror(errno));
+        printf("can't open\n");
         exit(1);
     }
     if(lockf(fd, F_TLOCK, 0) < 0){
         syslog(LOG_ERR, "can't lock %s: %s", LOCKFILE, strerror(errno));
+        printf("can't lock\n");
         exit(1);// a instance running
         }
     ftruncate(fd, 0);//将文件大小置为0
     sprintf(buf, "%ld", (long)getpid());
     write(fd, buf, strlen(buf)+1);
+    printf("excute ok once\n");
     exit(0);
 }
 
@@ -38,13 +42,11 @@ int main()
         printf("fork error\n");
     else if(pid == 0)
     {
-        printf("child process\n");
+        printf("child process, pid: %d\n",getpid());
         already_running();
     }
-    else
-    {
-       already_running();
-       printf("parent process\n");
-    }
+    
+    already_running();
+    printf("parent process, pid: %d\n",getpid());
     exit(0);
 }
